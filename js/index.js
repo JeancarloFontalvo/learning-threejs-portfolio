6c1828a5f8ecc90bfbf444c78a0cab5f13f22381
Jeancarlo Fontalvo <jeancarlofontalvo@gmail.com>
2022-01-23 20:31:47 -0500

    Hover effect part 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Find the latest version by visiting https://cdn.skypack.dev/three.
         
+import gsap from 'gsap'
 import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
 import * as dat from 'dat.gui'
 import {OrbitControls}  from 'https://cdn.skypack.dev/three@0.126.1/examples/jsm/controls/OrbitControls.js'
@@ -13,6 +14,13 @@ const world = {
         heightSegments: 10
     }
 }
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+const colors = []
 
 
 const scene = new THREE.Scene();
@@ -32,17 +40,27 @@ document.body.appendChild(renderer.domElement);
 
 const planeGeo = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
-    color: 0xff0000, 
     side: THREE.DoubleSide,
-    flatShading: THREE.FlatShading
+    flatShading: THREE.FlatShading,
+    vertexColors: true
 });
 const planeMesh = new THREE.Mesh(planeGeo, planeMaterial);
+for(let i = 0; i < planeMesh.geometry.attributes.position.count; i++){
+    colors.push(0, .19, .4)
+}
+
+planeMesh.geometry.setAttribute(
+    'color', 
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+)
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 
 const backlight = new THREE.DirectionalLight(0xffffff, 1);
 backlight.position.set(0, 0, -1);
+
+const raycaster = new THREE.Raycaster()
 
 scene.add(planeMesh);
 scene.add(light);
@@ -64,11 +82,20 @@ function animate(){
     requestAnimationFrame(animate);
     
     renderer.render(scene, camera);
+
+    raycaster.setFromCamera(mouse, camera);
+
+    intersectHovering(planeMesh)
+    
 }
 
-console.log(planeGeo.attributes.position.array)
-
 animate();
+
+
+addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX / innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+});
 
 
 function randomizeVertices(geometryArray){
@@ -92,5 +119,57 @@ function generatePlane() {
         world.plane.widthSegments, 
         world.plane.heightSegments)
 
+
+    planeMesh.geometry.setAttribute(
+        'color', 
+        new THREE.BufferAttribute(new Float32Array(colors), 3)
+    )
+
     randomizeVertices(planeMesh.geometry.attributes.position.array);
+}
+
+function intersectHovering(mesh){
+    const intersects = raycaster.intersectObject(mesh);
+
+    if(!intersects.length) 
+        return
+
+    let intersection = intersects[0];
+    console.log('intersecting')
+    const initialColor = {
+        r: 0,
+        g: .19,
+        b: .4
+    }
+
+    const hoverColor = {
+        r: .1,
+        g: .5,
+        b: 1
+    }
+
+    let {color} = intersection.object.geometry.attributes;
+    
+    colorVertices(hoverColor);
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true;
+
+    
+    gsap.to(hoverColor, {
+        r: initialColor.r,
+        g: initialColor.g,
+        b: initialColor.b,
+        onUpdate: () => {
+            colorVertices(hoverColor)
+        }
+    })
+
+    function colorVertices(colorRgb) {
+        
+        for (let group in intersection.face) {
+            color.setX(intersection.face[group], colorRgb.r);
+            color.setY(intersection.face[group], colorRgb.g);
+            color.setZ(intersection.face[group], colorRgb.b);
+        }
+    }
 }
